092cac0f4f3ec0700d60a46e9d11703f449c4852
Himanshu Singh <hsgbxrssm@gmail.com>
2024-08-01 13:59:36 +0530

    A blog app

---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { Container, PostCard } from '../components';
+import appwriteService from "../appwrite/config";
+
+function AllPosts() {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        appwriteService.getPosts()
+            .then((response) => {
+                if (response && response.documents) {
+                    setPosts(response.documents);
+                } else {
+                    throw new Error('Invalid response structure');
+                }
+            })
+            .catch((err) => {
+                setError(err.message);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    );
+}
+
+export default AllPosts;
